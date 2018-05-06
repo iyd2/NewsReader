bfe5b12078dcc058417a35da7319b8b72f8c6c88
ivan <fran.totti_96@mail.ru>
2018-05-06 22:18:34 +0300

    Create basic activity class for fragment hosting

---
+++ app/src/main/java/iyd2/projects/newsviewer/NewsRecyclerActivity.java
@@ -0,0 +1,14 @@
+package iyd2.projects.newsviewer;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsRecyclerActivity extends SingleActivityFragment {
+
+    @Override
+    public Fragment createFragment() {
+        return null;
+    }
+
+}
