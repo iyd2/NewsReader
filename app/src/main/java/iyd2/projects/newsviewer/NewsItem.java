654874179c58fa6a45231964611afdc1acbbfb89
ivan <fran.totti_96@mail.ru>
2018-05-07 23:37:58 +0300

    Add getters and setters

--- app/src/main/java/iyd2/projects/newsviewer/NewsItem.java
+++ app/src/main/java/iyd2/projects/newsviewer/NewsItem.java
@@ -9,9 +9,32 @@ public class NewsItem {
     private String urlToImage;
     private String publishedAt;
 
-    public NewsItem(String title, String description, String urlToImage) {
+    public NewsItem(String title, String description) {
         this.title = title;
         this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public void setUrlToImage(String urlToImage) {
         this.urlToImage = urlToImage;
     }
 }
