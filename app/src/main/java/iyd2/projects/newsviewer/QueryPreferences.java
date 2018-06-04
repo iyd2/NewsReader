c8192f552db02c3a6801ff232ada151014e9f705
ivan <fran.totti_96@mail.ru>
2018-06-04 23:16:05 +0300

    Add class to work with SharedPreferences

---
+++ app/src/main/java/iyd2/projects/newsviewer/QueryPreferences.java
@@ -0,0 +1,34 @@
+package iyd2.projects.newsviewer;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class QueryPreferences {
+    private static final String TAG = "QueryPreferences";
+    private static final String LAST_DATE_QUERY = "last_date";
+
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd'T'HH:mm:ss");
+
+    public static Date getLastDateQuery(Context context) {
+        String date = PreferenceManager.getDefaultSharedPreferences(context).getString(LAST_DATE_QUERY, null);
+        try {
+            return dateFormat.parse(date);
+        } catch (ParseException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        return null;
+    }
+
+    public static void setLastDateQuery(Context context, Date value) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(LAST_DATE_QUERY, dateFormat.format(value))
+                .apply();
+    }
+}
