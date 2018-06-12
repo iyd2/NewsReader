f9b3c916e26f5fa4871ae3e5231c2ea1d98c132e
ivan <fran.totti_96@mail.ru>
2018-06-12 14:13:08 +0300

    Add logs

--- app/src/main/java/iyd2/projects/newsviewer/SingleFragmentActivity.java
+++ app/src/main/java/iyd2/projects/newsviewer/SingleFragmentActivity.java
@@ -6,20 +6,25 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    private static final String TAG = "SingleFragmentActivity";
 
     public abstract Fragment createFragment();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.fragment_activity);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
+            Log.d(TAG, "create fragment");
             fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
